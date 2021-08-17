d471eaf99eb4cc60e108484e612d86e09af5602d
RAKOTOMAMONJY Sitraka <mbolasitrakaa@gmail.com>
2021-08-17 15:11:30 +0200

    add create fonctionality

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,18 +1,35 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Crud;
+use App\Form\CrudType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'main')]
+    #[Route('/', name: 'main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+    #[Route('/create', name: 'create')]
+    public function create(Request $request){
+        $crud = new Crud();
+        $form = $this->createForm(CrudType::class,$crud);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($crud);
+            $em->flush();
+            $this->addFlash('notice','Submitted Successfully');
+        }
+        return $this->render('main/create.html.twig',[
+            'form' => $form->createView()
         ]);
     }
 }
